57191fd3d2e904508f43de71fae86ca47f224618
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-05-16 02:23:44 +0600

    Update active_mess_provider.dart

--- lib/features/mess/controllers/active_mess_provider.dart
+++ lib/features/mess/controllers/active_mess_provider.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../auth/controllers/auth_controller.dart';
 
-// This provider listens to Firebase to see if the user is part of ANY mess.
 final activeMessProvider = StreamProvider<String?>((ref) {
   final user = ref.watch(authStateProvider).value;
   if (user == null) return Stream.value(null);
@@ -13,9 +12,8 @@ final activeMessProvider = StreamProvider<String?>((ref) {
       .snapshots()
       .map((snapshot) {
     if (snapshot.docs.isNotEmpty) {
-      // Return the ID of the mess they belong to
       return snapshot.docs.first.reference.parent.parent?.id;
     }
-    return null; // User is not in a mess
+    return null;
   });
 });
