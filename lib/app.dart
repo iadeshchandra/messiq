deafb03082de1924e77bbf2f284c7f713b365f9a
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-05-15 13:31:32 +0600

    Create lib/app.dart

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:messiq/core/theme/app_theme.dart';
+import 'package:messiq/features/auth/views/auth_gate.dart';
+
+class MessIqApp extends StatelessWidget {
+  const MessIqApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MessIQ',
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      home: const AuthGate(), // Now uses AuthGate to manage sessions
+    );
+  }
+}
